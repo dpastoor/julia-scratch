d721f6aabbd384983695b1c4b8bcccae4c534344
Devin Pastoor <devin.pastoor@gmail.com>
2015-02-16 21:16:10 -0500

    remove extraneous printlns

--- cleannm1.jl
+++ cleannm1.jl
@@ -1,33 +1,6 @@
 using ArgParse
 
-function cleannm(filename, headerrow=-99)
-    rawdat = readlines(open(filename))
-    keeplines = similar(rawdat[3:4], 0)
-    println(headerrow)
-    if headerrow .== -99
-        for (i, x) in enumerate(rawdat)
-            if headerrow .== -99
-                if ismatch(r"ID|DV|TIME|MDV|EVID",x)
-                    headerrow = i 
-                    break
-                end
-                continue
-            end
-        end
-    end
-    println(headerrow)
-    for x in rawdat
-        if(!ismatch(r"(TABLE|ID)", x)) 
-            push!(keeplines, lstrip(x))
-        end
-    end
-    wdat = [replace(rawdat[headerrow], " ", ""), keeplines]
-    c = open(filename*"cleaned", "w+")
-    write(c, wdat)
-    close(c)
-    println("successfully cleaned")
-end
-
+include("cleannm_function.jl")
 
 
 s = ArgParseSettings("clean nonmem csv tables: " *  # description
@@ -45,9 +18,6 @@ s = ArgParseSettings("clean nonmem csv tables: " *  # description
 end
 
 parsed_args = parse_args(ARGS, s)
-println("Parsed args:")
-println(parsed_args["arg1"])
-println(parsed_args["hr"])
 println("Running cleannm")
 cleannm(parsed_args["arg1"], parsed_args["hr"])
 
