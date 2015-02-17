4d84a53c6beca3a69dc312df0c7bcf5176f77fdb
Devin Pastoor <devin.pastoor@gmail.com>
2015-02-16 21:17:04 -0500

    clean up version with overwrite

--- cleannm2.jl
+++ cleannm2.jl
@@ -1,34 +1,6 @@
 using ArgParse
 
-function cleannm(filename, headerrow=2, overwrite = false)
-    rawdat = readlines(open(filename))
-    keeplines = similar(rawdat[3:4], 0)
-    if headerrow .== -99
-        for (i, x) in enumerate(rawdat)
-            if headerrow .== -99
-                if ismatch(r"ID|DV|TIME|MDV|EVID",x)
-                    headerrow = i 
-                    break
-                end
-                continue
-            end
-        end
-    end
-    for x in rawdat
-        if(!ismatch(r"ID|DV|TIME|MDV|EVID", x)) 
-            push!(keeplines, lstrip(x))
-        end
-    end
-    wdat = [replace(rawdat[headerrow], " ", ""), keeplines]
-    if overwrite
-        c = open(filename, "w+")
-    else
-        c = open(filename*"cleaned", "w+")
-    end
-    write(c, wdat)
-    close(c)
-    println("successfully cleaned")
-end
+include("cleannm_function.jl")
 
 
 
@@ -51,10 +23,7 @@ s = ArgParseSettings("clean nonmem csv tables: " *  # description
 end
 
 parsed_args = parse_args(ARGS, s)
-println("Parsed args:")
-println(parsed_args["arg1"])
-println(parsed_args["hr"])
-println("Running cleannm")
+println("Running cleannm...")
 cleannm(parsed_args["arg1"], parsed_args["hr"], parsed_args["overwrite"])
 
 
